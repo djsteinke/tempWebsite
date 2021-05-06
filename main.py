a1486de7c7257dc31f7a1e3f8b1fe9ecc1a9155b
ds111g <ds111g@att.com>
2021-05-06 15:42:03 -0600

    add ip

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'templates'), 'style.css')
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     host_name = socket.gethostbyname(socket.gethostname())
