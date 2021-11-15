00fecf81e713c7ea41acd03ced94abfb4a97f774
ds111g <ds111g@att.com>
2021-11-15 13:58:17 -0700

    horizontal

--- main.py
+++ main.py
@@ -2,11 +2,56 @@ import os
 import re
 import socket
 from flask_cors import CORS
+from PIL import Image, ImageDraw, ImageFont
+from PIL.ImageFont import load_default
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, url_for
 
 app = Flask(__name__)
 CORS(app)
+
+
+@app.route('/img')
+def get_image():
+    w = 758
+    h = 758
+    img = Image.new('RGB', (w, h), color="#FFFFFF")
+    canvas = ImageDraw.Draw(img)
+
+    font = ImageFont.truetype('Roboto-Regular.ttf', 50)
+    text_width, text_height = canvas.textsize('Outside', font=font)
+    x_pos = int((w - text_width) / 2)
+    y_pos = int(text_height/2)
+    canvas.text((x_pos, y_pos), "Outside", font=font, fill='#000000')
+
+    font = ImageFont.truetype('Roboto-Regular.ttf', 30)
+    text_width, text_height = canvas.textsize('Temperature', font=font)
+    x_pos = int(w/4 - text_width/2)
+    y_pos = int(y_pos + 50 + (text_height*1.1))
+    canvas.text((x_pos, y_pos), "Temperature", font=font, fill='#000000')
+
+    font = ImageFont.truetype('Roboto-Regular.ttf', 30)
+    text_width, text_height = canvas.textsize('Humidity', font=font)
+    x_pos = int(3*w/4 - text_width/2)
+    canvas.text((x_pos, y_pos), "Humidity", font=font, fill='#000000')
+
+    font = ImageFont.truetype('Roboto-Regular.ttf', 100)
+    text_width, text_height = canvas.textsize('T Val', font=font)
+    x_pos = int(w/4 - text_width/2)
+    y_pos = int(y_pos + 50 )
+    canvas.text((x_pos, y_pos), "T Val", font=font, fill='#000000')
+
+    font = ImageFont.truetype('Roboto-Regular.ttf', 100)
+    text_width, text_height = canvas.textsize('H Val', font=font)
+    x_pos = int(3*w/4 - text_width/2)
+    canvas.text((x_pos, y_pos), "H Val", font=font, fill='#000000')
+
+    img.rotate(90).show()
+
+    img.save('static/image.jpg')
+    v = '<img src=' + url_for('static', filename='image.jpg') + '>'
+    print(v)
+    return 'value'
 
 
 @app.route('/')
